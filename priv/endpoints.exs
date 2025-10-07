9843b112bf57cd2fd050682114aae9ea1f2a7a15
Konrad Zemek <konrad@v7labs.com>
2025-10-07 16:41:23 +0200

    Add servicequotas

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -188,6 +188,33 @@ chime_voice_regions = [
             "us-west-2" => %{}
           }
         },
+        "servicequotas" => %{
+          "defaults" => %{"credentialScope" => %{"service" => "servicequotas"}},
+          "endpoints" => %{
+            "ap-northeast-1" => %{},
+            "ap-northeast-2" => %{},
+            "ap-northeast-3" => %{},
+            "ap-south-1" => %{},
+            "ap-south-2" => %{},
+            "ap-southeast-1" => %{},
+            "ap-southeast-2" => %{},
+            "ca-central-1" => %{},
+            "eu-central-1" => %{},
+            "eu-central-2" => %{},
+            "eu-north-1" => %{},
+            "eu-south-1" => %{},
+            "eu-south-2" => %{},
+            "eu-west-1" => %{},
+            "eu-west-2" => %{},
+            "eu-west-3" => %{},
+            "sa-east-1" => %{},
+            "us-east-1" => %{},
+            "us-east-2" => %{},
+            "us-gov-east-1" => %{},
+            "us-gov-west-1" => %{},
+            "us-west-2" => %{}
+          }
+        },
         "bedrock-agent" => %{
           "defaults" => %{"credentialScope" => %{"service" => "bedrock"}},
           "endpoints" => %{
